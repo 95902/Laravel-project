d66a276dfb5572923e02a38c0a786cdbef3a123b
95902 <bragance.m@hotmail.fr>
2023-09-06 15:38:09 +0200

    modules laravel #3

--- example-app/tests/Feature/ExampleTest.php
+++ example-app/tests/Feature/ExampleTest.php
@@ -2,18 +2,26 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+use function Pest\Laravel\{get};
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+it('has a welcome page', function () {
 
-        $response->assertStatus(200);
-    }
-}
+    get('/')->assertok();
+
+});
+
+// // use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Tests\TestCase;
+
+// class ExampleTest extends TestCase
+// {
+//     /**
+//      * A basic test example.
+//      */
+//     public function test_the_application_returns_a_successful_response(): void
+//     {
+//         $response = $this->get('/');
+
+//         $response->assertStatus(200);
+//     }
+// }
